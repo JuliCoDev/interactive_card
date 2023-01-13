c9c4a89b019b786d07ec439f7a58b880fa2571b4
JulianaGarciaCo <julianageraldineco@gmail.com>
2023-01-12 22:09:57 -0500

    Add input style component

---
+++ src/styleComponents/Input.jsx
@@ -0,0 +1,13 @@
+import tw from "tailwind-styled-components"
+
+const Input = tw.input`  
+    outline-none
+    border
+    border-gray-200
+    focus:border-semi-darck
+    rounded-md
+    p-2
+    
+`;
+
+export default Input;
