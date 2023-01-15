f1000c011be28f8916d82a4121f233d7f6991b54
JulianaGarciaCo <julianageraldineco@gmail.com>
2023-01-15 12:31:01 -0500

    Add format to inputs to user can't enter a incorrect format

---
+++ src/hooks/useFormatInputCard.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useFormatInputCard = (cardValues) =>{
+    
+    const [formatCardInput , setFormatCardNumber] = useState(cardValues);
+
+
+    const changeFormatOnlyNumbers = (name,text) => {
+        let textFormat =  text?.replace(/\s/g,'')//Quita espacios en blanco
+        .replace(/[^0-9]/g, '')
+
+        
+        setFormatCardNumber({...formatCardInput,
+            [name] : textFormat
+        })
+    }
+
+
+    const changeFormatCardNumber = (name, text) =>{
+
+        let textFormat =  text?.replace(/\s/g,'')//Quita espacios en blanco
+        .replace(/[^0-9]/g, '')
+        .replace(/([0-9]{4})/g, '$1 ')//Coloca un espacio cada 4 numeros
+        
+        setFormatCardNumber({...formatCardInput,
+            [name] : textFormat?.trim()
+        })
+
+    }
+
+    
+    return{
+        formatCardInput,
+        changeFormatOnlyNumbers,
+        changeFormatCardNumber,
+    }
+}
+
+export default useFormatInputCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,50 @@
 import Input from "../styleComponents/Input";
 import Label from "../styleComponents/Label";
 import Button from "../styleComponents/Button";
+import useFormatInputCard from "../hooks/useFormatInputCard";
+import { useEffect, useState } from "react";
+
+const initialValues = {
+    cardholderName: '',
+    cardNumber: '',
+    month: '',
+    year: ''
+} 
 
 
 export default function Dashboard(){
+
+    const [ cardValues , setCardValues ] = useState(initialValues);
+
+    const {
+        formatCardInput,
+        changeFormatOnlyNumbers,
+        changeFormatCardNumber,
+    } = useFormatInputCard(cardValues);
+    
+
+    const handleChange = (e) =>{
+        setCardValues({
+            [e.target.name] : e.target.value
+        })   
+        
+        if([e.target.name] == "cardNumber"){
+            changeFormatCardNumber("cardNumber" , e.target.value);  
+        }else if([e.target.name] !== "cardholderName"){
+            changeFormatOnlyNumbers([e.target.name] , e.target.value)
+          
+        }
+    }
+
+
     
     return(
         <form className="w-4/5 m-auto mt-4">
+            
+
             <Label htmlFor="cardholderName">CARDHOLDER NAME</Label>
             <Input 
+                onChange={(e) => handleChange(e)} 
                 name="cardholderName" 
                 placeholder="e.g Jane Appleseed" 
                 type="text"              
@@ -16,10 +52,12 @@ export default function Dashboard(){
 
             <Label htmlFor="cardNumber">CARD NUMBER</Label>
             <Input 
-                nameInput="cardNumber" 
+                onChange={(e) => handleChange(e) } 
+                name="cardNumber" 
                 placeholder="e.g. 1234 5678 9123 0000"
                 type="text"   
-                maxLength={19}                           
+                maxLength={19}     
+                value={formatCardInput.cardNumber}                          
             />
             
             <div className="flex justify-between ">               
@@ -28,32 +66,37 @@ export default function Dashboard(){
                     <div className="flex justify-between">
                         <div className="inline-block w-2/4 box-border pr-2" >
                             <Input 
+                                onChange={(e) => handleChange(e)} 
                                 name="month" 
                                 placeholder="MM" 
                                 type="text"
                                 maxLength={2}     
-                                   
+                                value={formatCardInput.month}    
 
                             />
                         </div>
                         <div className="inline-block w-2/4" >
                             <Input 
+                                onChange={(e) => handleChange(e)} 
                                 name="year" 
                                 placeholder="YY" 
                                 type="text"    
                                 maxLength={2} 
+                                value={formatCardInput.year}                  
                             />
                         </div>
                     </div>
                 </div>
 
                 <div className="w-2/4 box-border pl-2 ">
-                    <Label className="block">Cvc</Label>
+                    <Label className="block">CVC</Label>
                     <Input 
+                        onChange={(e) => handleChange(e)} 
                         name="cvc" 
                         placeholder="e.g. 123" 
                         type="text"    
-                        maxLength={2} 
+                        maxLength={3} 
+                        value={formatCardInput.cvc}    
                     />
                 </div>
             </div>
